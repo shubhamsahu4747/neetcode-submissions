415dc0ec83534796cb815aaf540a382072d75c3a
Shubham Sahu <115063193+shubhamsahu4747@users.noreply.github.com>
2026-09-19 12:59:19 +0530

    Add: top-k-elements-in-list - submission-3

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-3.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+      
+        // prepared the map
+        unordered_map<int, int>count;
+        for(int num:nums){
+            count[num]++;
+        }
+
+        //Maintained the frequncey of each char  
+        vector<pair<int, int>> arr;
+        for( const auto& p:count){
+            arr.push_back({p.second, p.first});
+        } 
+        sort(arr.rbegin(),arr.rend());
+
+        vector<int> res;
+        for(int i =0;i < k;++i){
+           res.push_back(arr[i].second);     
+        }
+
+        return res;
+    }
+};
