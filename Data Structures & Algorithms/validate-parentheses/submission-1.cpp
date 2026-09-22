fd0434ce29d0081a85a10de5f8eac1f9ea18d035
Shubham Sahu <115063193+shubhamsahu4747@users.noreply.github.com>
2026-09-22 11:54:37 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> myStack;
+        unordered_map<char ,char>closeToOpen ={
+            {')','('},
+            {']','['},
+            {'}','{'},
+        };
+        for(char c:s){
+            if(closeToOpen[c]){
+                if(!myStack.empty() && myStack.top() == closeToOpen[c]){
+                    myStack.pop();
+                }else{
+                    return false;
+                }
+            }else{
+                myStack.push(c);
+            }
+        }
+        return myStack.empty();
+    }
+};
